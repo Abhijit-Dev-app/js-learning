0fa3fad873fd815c0d5719bf24469575361ca1c3
Abhijit-Dev-app <solemrec@gmail.com>
2026-01-09 09:39:08 +0000

    ConversionOfDatatypes

---
+++ .devcontainer/basics_js/02_ConversionOperation.js
@@ -0,0 +1,25 @@
+let score  = "33";
+// let score = null;
+// console.log(typeof score);
+// console.log(typeof (score));
+// two ways to print type of anydata
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// 33 -> 33
+// "33abc" -> NaN(Not a Number)
+
+let isLoggedIn = 1;
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
+// 1=> true;
+// ""-> false
+// "Abhijit" -> true;
+
+let string  = 33;
+let strNumber = String(string);
+console.log(typeof strNumber);
+console.log(strNumber);
+
+
+
