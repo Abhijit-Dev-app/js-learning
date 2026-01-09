741b436faaf97717917bcf9c08a810e40d616a6b
Abhijit-Dev-app <solemrec@gmail.com>
2026-01-09 10:43:47 +0000

    Done

--- .devcontainer/basics_js/02_ConversionOperation.js
+++ .devcontainer/basics_js/02_ConversionOperation.js
@@ -1,4 +1,4 @@
-let score  = "33";
+let score  = "33"
 // let score = null;
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -9,33 +9,32 @@ let score  = "33";
 // 33 -> 33
 // "33abc" -> NaN(Not a Number)
 
-let isLoggedIn = 1;
-let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanIsLoggedIn);
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
 // 1=> true;
 // ""-> false
 // "Abhijit" -> true;
 
-let string  = 33;
-let strNumber = String(string);
-console.log(typeof strNumber);
-console.log(strNumber);
+let string  = 33
+let strNumber = String(string)
+console.log(typeof strNumber)
+console.log(strNumber)
 
 // ****************************Operation*****************************************
-let  vlaue = 3;
-let negValue = -vlaue;
+let  vlaue = 3
+let negValue = -vlaue
 // console.log(negValue);
 // console.log(2 + 2);
 // console.log(2**3);
 
 let str1 = "hello"
-let str2 = "hello2";
+let str2 = "hello2"
 // console.log(str1 + str2);
 // console.log(+true); -> 1
-console.log(true);
+console.log(true)
 // console.log(+""); ->0
-let x = 3;
-let y = 4;
-console.log(`x:${x}, y:${y}`);
-
+let x = 3
+let y = 4
+console.log(`x:${x}, y:${y}`)
 
---
+++ .devcontainer/basics_js/Data_type.js
@@ -0,0 +1,28 @@
+// Primitive datatype
+// String
+// Number, boolean, null, undefined, symbol, BigInt
+// Reference type-> (Non primitie)
+// Array, objects, functions,
+// ->
+const score = 100;
+const numval = 100.3;
+const isloggedIn = false;
+const outsidetemp = null;
+let userEmail;/*value if we print will be undefined*/
+const id = Symbol('123');
+const anotherid = Symbol('123');
+console.log(id === anotherid);
+
+
+const heroes = ["ironman", "nagraj", "doga"];
+let myobj = {
+    name:"Abhijit",
+    age: 23
+}
+
+const myfun = function(){
+    console.log("Hello world");
+}
+// let store = null; -> object (imp)
+// console.log(typeof store)
+console.log(typeof heroes)
